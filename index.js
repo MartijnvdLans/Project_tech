7da3c2dc109e0b521a523ab94dada84c33fac373
MartijnvdLans <Martijnvanderlans.00@gmail.com>
2021-04-22 13:31:46 +0200

    test

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-});
+})
+
+app.get('/series', (req, res) => {
+  res.send('Lijst met series')
+})
 
 app.get('/movies', (req, res) => {
-  res.send('<h1>This will become a list of movies</h1>')
-});
-
-app.get('/movies/:movieId/:slug', (req, res) => {
-  res.send(`<h1>This will become a detail page for ${req.params.slug}</h1>`)
-});
-
-app.use(function (req, res, next) {
-  res.status(404).send("This page doen not exsist!")
-});
+  res.send('<h1>This will be a list of movies!</h1>')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
-});
+})
