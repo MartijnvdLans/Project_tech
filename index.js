5274c9fd02b8939e9153d254d443e8e1edeb1ae3
MartijnvdLans <Martijnvanderlans.00@gmail.com>
2021-04-22 13:47:32 +0200

    Server blijft aanstaan?

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/series', (req, res) => {
-  res.send('Lijst met series')
-})
-
 app.get('/movies', (req, res) => {
   res.send('<h1>This will be a list of movies!</h1>')
+})
+
+app.get('/movies/:movieId/:slug', (req, res) => {
+  res.send(`<h1>This is the detail page of ${req.params.slug}!</h1>`)
 })
 
 app.listen(port, () => {
