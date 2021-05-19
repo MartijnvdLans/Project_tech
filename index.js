71fbc131e8ffbe925632d0cc84cc32a1c4e2fc4b
MartijnvdLans <Martijnvanderlans.00@gmail.com>
2021-05-19 21:24:35 +0200

    heroku

--- index.js
+++ index.js
@@ -49,9 +49,5 @@ app.use(function (req, res, next) {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-})
-
-app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
