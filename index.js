1effde0e2f1f8509d2e7b96c4abafc9c57c39253
MartijnvdLans <Martijnvanderlans.00@gmail.com>
2021-05-26 19:53:38 +0200

    server geeft nu aan of de persoon wordt geliked of niet

--- index.js
+++ index.js
@@ -37,11 +37,27 @@ app.get('/', async (req, res) => {
   res.render('home', {title:"NetMatch", users})
 })
 
-app.post('/', async (req, res) => {
+app.post('/liked/*', async (req, res) => {
   console.log(req.body.id)
+  const query = {};
+  const options = {sort: {age: +1}};
+  const users = await db.collection('users').find(query, options).toArray();
+  res.render('home', {title:"NetMatch", users})
   // Stap 1: in database opslaan dat de huidige gebruiker iemand anders liked
   // Stap 2: Id like opslaan
-  // Stap 3: Controleren of de gebruikr mij ook liked
+  // Stap 3: Controleren of de gebruiker mij ook liked
+  // Stap x: Pagina renderen afhankelijk van op de nieuwe gebruiker geliked wordt of animatie
+})
+
+app.post('/disliked/*', async (req, res) => {
+  console.log(req.body.id)
+  const query = {};
+  const options = {sort: {age: +1}};
+  const users = await db.collection('users').find(query, options).toArray();
+  res.render('home', {title:"NetMatch", users})
+  // Stap 1: in database opslaan dat de huidige gebruiker iemand anders liked
+  // Stap 2: Id like opslaan
+  // Stap 3: Controleren of de gebruiker mij ook liked
   // Stap x: Pagina renderen afhankelijk van op de nieuwe gebruiker geliked wordt of animatie
 })
 
